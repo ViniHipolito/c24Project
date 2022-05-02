68801d05fb494ebb72b94e678051c71b7b91e529
ViniHipolito <97119997+ViniHipolito@users.noreply.github.com>
2022-05-02 17:24:21 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,84 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var palyer, playerBase, playerArcher;
+var playerArrows = [];
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  angleMode(DEGREES);
+
+  var options = {
+    isStatic: true
+  };
+
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
+
+  player = Bodies.rectangle(250, playerBase.position.y - 150, 50, 180, options);
+  World.add(world,player)
+
+  playerArcher = new PlayerArcher(
+    335,
+    playerBase.position.y - 90,
+    120,
+    120
+  );
+}
+
+function draw() {
+  background(backgroundImg);
+
+  Engine.update(engine);
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+
+  playerArcher.display();
+
+    for (var i = 0; i < playerArrows.length; i++) {
+      if (playerArrows[i] !== undefined) {
+        playerArrows[i].display();
+      }
+    }
+    image(playerimage,player.position.x,player.position.y,50,180)
+
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("Dummy Épico", width / 2, 100);
+}
+
+function keyPressed() {
+if (keyCode === LEFT_ARROW) {
+var posX = playerArcher.body.position.x;
+var posY = playerArcher.body.position.y;
+var angle = playerArcher.body.angle;
+var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+Matter.Body.setAngle(arrow.body, angle);
+playerArrows.push(arrow);
+   }
+ }
+
+function keyReleased() {
+  if (keyCode === LEFT_ARROW) {
+    if (playerArrows.length) {
+      var angle = playerArcher.body.angle;
+      playerArrows[playerArrows.length - 1].shoot(angle);
+    }
+  }
+}
